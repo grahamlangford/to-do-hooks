afcffd5ee64c3a345725bbec96c4b1cb59e84c07
Graham Langford <grahamlangford@Grahams-MacBook-Pro.local>
2019-05-06 11:57:19 -0500

    brings back testing of todo list rendering

--- src/components/ListContext/ListContext.spec.js
+++ src/components/ListContext/ListContext.spec.js
@@ -4,14 +4,25 @@ import ToDoList from '.'
 import { TodoProvider } from '../../context/todoContext'
 
 describe('ToDoList.jsx', () => {
+  const testTodos = [
+    { id: '0', value: 'test-0', checked: false },
+    { id: '1', value: 'test-1', checked: true }
+  ]
+
   const wrapper = () =>
     render(
-      <TodoProvider>
+      <TodoProvider value={[testTodos, () => {}]}>
         <ToDoList />
       </TodoProvider>
     )
 
   it('renders without crashing', () => {
     wrapper()
+  })
+
+  it('renders a list of todos', () => {
+    const { getAllByText } = wrapper()
+    const todos = getAllByText('test', { exact: false })
+    expect(todos).toHaveLength(2)
   })
 })
