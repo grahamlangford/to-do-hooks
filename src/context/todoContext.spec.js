c171d0a891c8aecd285f61324759080d5ffb49e9
Graham Langford <grahamlangford@Grahams-MacBook-Pro.local>
2019-05-06 16:55:29 -0500

    cleaned up useTodo tests

--- src/context/todoContext.spec.js
+++ src/context/todoContext.spec.js
@@ -6,22 +6,25 @@ describe('context/todoContext.jsx', () => {
     { id: '0', value: 'test-0', checked: false },
     { id: '1', value: 'test-1', checked: true }
   ]
+  beforeEach(() => {
+    window.localStorage.setItem('todos-context', JSON.stringify(testTodos))
+  })
 
   it('addTodo adds a new todo to the array', () => {
     const { result } = renderHook(() => useTodo(), { wrapper: TodoProvider })
 
-    expect(result.current.todos).toEqual([])
-    act(() => result.current.addTodo(testTodos[0]))
+    expect(result.current.todos).toEqual(testTodos)
+    const newTodo = { id: '3', value: 'test-3', checked: true }
 
-    expect(result.current.todos).toEqual([testTodos[0]])
+    act(() => result.current.addTodo(newTodo))
+
+    expect(result.current.todos).toEqual([...testTodos, newTodo])
   })
 
   it('updateTodo replaces a todo with a new version', () => {
     const newTodo = { id: '1', value: 'updated value', checked: false }
 
     const { result } = renderHook(() => useTodo(), { wrapper: TodoProvider })
-    act(() => result.current.addTodo(testTodos[0]))
-    act(() => result.current.addTodo(testTodos[1]))
     expect(result.current.todos).toEqual(testTodos)
 
     act(() => result.current.updateTodo(newTodo))
@@ -30,8 +33,6 @@ describe('context/todoContext.jsx', () => {
 
   it('deleteTodo removes the todo by its id', () => {
     const { result } = renderHook(() => useTodo(), { wrapper: TodoProvider })
-    act(() => result.current.addTodo(testTodos[0]))
-    act(() => result.current.addTodo(testTodos[1]))
     expect(result.current.todos).toEqual(testTodos)
 
     act(() => result.current.deleteTodo('0'))
